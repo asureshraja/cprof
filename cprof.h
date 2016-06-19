4b9f5fda7dfcfd3b446ebeae3930aeb232b9140b
suresh <sureshraja316@gmail.com>
2016-06-19 12:41:56 +0530

    added testing support. experimenting with libunwind

--- cprof.h
+++ cprof.h
@@ -9,14 +9,19 @@
 #include <sys/syscall.h>
 #include <pthread.h>
 #include <string.h>
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
 #include <dlfcn.h>
 #include <sys/time.h>
+#include <stdarg.h>
+#include <stddef.h>
 #include <sys/times.h>
 #include <sys/vtimes.h>
 #include <execinfo.h>
 #include <mcheck.h>
 #define READ 0
 #define WRITE 1
+#define _init_cprof_   signal(SIGSEGV, signal_callback_handler);
 #define _show_line_info_ printf("Thread:%ld File:%s Function:%s Line:%d\n",pthread_self(),__FILE__,__FUNCTION__,__LINE__ );
 #define _show_res_usage_info_ get_res_usage();
 #define _print_virtmem_usage_ printf("Virtual Memory usage:%dkb\n",getVirtualMemoryUsage());
@@ -27,7 +32,17 @@
 #define _stop_time_trace_ pclose2(p);
 #define _start_detail_trace_ char buf[1024]; sprintf (buf, "sudo ltrace -S -x -tt -n 5 -b -p %d",getpid()); int r=0; int w=1; pid_t p = popen2(buf, &r, &w); sleep(1);
 #define _stop_detail_trace_ pclose2(p);
-
+#define _start_testing_(x)  int __;for (__ = 0; __ < x; ++__){
+#define _start_cases_ switch (__){
+#define _start_case_(x) case x:{printf("\nExecuting test case %d block\n",__);
+#define _stop_case_ printf("End of test case %d Execution\n",__ );}break;
+#define _stop_cases_ default:printf("%s\n","No test case executed. Wrong test case number." );break;}
+#define _start_verification_ switch (__){printf("\nExecuting test case %d Verification block\n",__);
+#define _start_case_verify_(x) case x:{printf("starting test case %d verification\n",__ );
+#define _stop_case_verify printf("End of test case %d verification\n",__ );}break;
+#define _stop_verification_ default:printf("%s\n","Verification Failed." );break;}
+#define _stop_testing_ }
+#define _show_backtrace_ show_backtrace();
 pid_t
 popen2(const char *command, int *infp, int *outfp)
 {
@@ -128,4 +143,164 @@ void get_res_usage(){
         getrusage(RUSAGE_THREAD,res);
         printf("\nUser Mode Time Spent:%ld\nKernel Mode Time Spent:%ld\nMaximum Resident Set size:%ldkb\nNo of Page Faults Serviced Without I/O:%ld\nNo of Page Faults Serviced that required I/O:%ld\nNo of FileSystem Input Operation:%ld\nNo of FileSystem Output OP:%ld\nNo of Voluntary Context Switches:%ld\nNo of Involuntary Context Switches:%ld\n",res->ru_stime.tv_sec,res->ru_utime.tv_sec,res->ru_maxrss,res->ru_minflt,res->ru_majflt, res->ru_inblock,res->ru_oublock,res->ru_nvcsw,res->ru_nivcsw);
         free(res);
+}
+
+int getFileAndLine (unw_word_t addr, char *file, size_t flen, int *line)
+{
+	char buf[1024];
+	char *p;
+
+	// prepare command to be executed
+	// our program need to be passed after the -e parameter
+	sprintf (buf, "/usr/bin/addr2line -C -e ./a.out -f -i %lx", addr);
+	FILE* f = popen (buf, "r");
+
+	if (f == NULL)
+	{
+		perror (buf);
+		return 0;
+	}
+
+	// get function name
+	fgets (buf, 1024, f);
+
+	// get file and line
+	fgets (buf, 1024, f);
+
+	if (buf[0] != '?')
+	{
+		int l;
+		char *p = buf;
+
+		// file name is until ':'
+		while (*p != ':')
+		{
+			p++;
+		}
+
+		*p++ = 0;
+		// after file name follows line number
+		strcpy (file , buf);
+		sscanf (p,"%d", line);
+	}
+	else
+	{
+		strcpy (file,"unkown");
+		*line = 0;
+	}
+
+	pclose(f);
+}
+
+void show_backtrace ()
+{
+	char name[256];
+	unw_cursor_t cursor; unw_context_t uc;
+	unw_word_t ip, sp, offp;
+
+	unw_getcontext(&uc);
+	unw_init_local(&cursor, &uc);
+
+	while (unw_step(&cursor) > 0)
+	{
+		char file[256];
+		int line = 0;
+		name[0] = '\0';
+		unw_get_proc_name(&cursor, name, 256, &offp);
+		unw_get_reg(&cursor, UNW_REG_IP, &ip);
+		unw_get_reg(&cursor, UNW_REG_SP, &sp);
+		//printf ("%s ip = %lx, sp = %lx\n", name, (long) ip, (long) sp);
+		getFileAndLine((long)ip, file, 256, &line);
+		printf("%s in file %s line %d\n", name, file, line-1);
+	}
+}
+void sigsegv_show_backtrace ()
+{
+	char name[256];
+	unw_cursor_t cursor; unw_context_t uc;
+	unw_word_t ip, sp, offp;
+
+	unw_getcontext(&uc);
+	unw_init_local(&cursor, &uc);
+
+	while (unw_step(&cursor) > 0)
+	{
+		char file[256];
+		int line = 0;
+		name[0] = '\0';
+		unw_get_proc_name(&cursor, name, 256, &offp);
+		unw_get_reg(&cursor, UNW_REG_IP, &ip);
+		unw_get_reg(&cursor, UNW_REG_SP, &sp);
+		//printf ("%s ip = %lx, sp = %lx\n", name, (long) ip, (long) sp);
+		getFileAndLine((long)ip, file, 256, &line);
+		printf("%s in file %s line %d\n", name, file, line-2);
+	}
+}
+
+void
+unwind_n(size_t depth, size_t n,...)
+{
+    int i, j;
+    va_list ap;
+    struct a { struct a *next; } *ebp;
+    struct {
+        size_t size;
+        char *addr;
+        ptrdiff_t offset;
+    } vars[n];
+
+#if __x86_64__
+    asm volatile("movq      %%rbp, %0;" : "=r" (ebp));
+#else
+    asm volatile("movl      %%ebp, %0;" : "=r" (ebp));
+#endif
+
+    ebp = ebp->next;
+
+    va_start(ap, n);
+    for (i = 0; i < n; i++) {
+        vars[i].offset = (vars[i].addr = va_arg(ap, char *))
+            	- (char *)ebp;
+        vars[i].size = va_arg(ap, size_t);
+    }
+    va_end(ap);
+
+    for (i = 0; i < depth; i++)
+        ebp = ebp->next;
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < vars[i].size; j++)
+            vars[i].addr[j] = (((char *)ebp + vars[i].offset))[j];
+}
+
+void
+unwind(size_t depth, char *addr, size_t len)
+{
+    size_t i;
+    struct a { struct a *next; } *ebp;
+    ptrdiff_t offset;
+
+#if __x86_64__
+    asm volatile("movq      %%rbp, %0;" : "=r" (ebp));
+#else
+    asm volatile("movl      %%ebp, %0;" : "=r" (ebp));
+#endif
+
+    /* Enter the first stack frame and get the offset of the variable. */
+    ebp = ebp->next;
+    offset = addr - (char *)ebp;
+
+    for (i = 0; i < depth; i++)
+        ebp = ebp->next;
+
+    for (i = 0; i < len; i++){
+        addr[i] = (((char *)ebp + offset))[i];
+        printf("%d\n",*addr );
+
+    }
+}
+void signal_callback_handler(int signum)
+{
+   sigsegv_show_backtrace();
+   exit(signum);
 }
--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include "cprof.h"
+
 int meow(int a,int b);
 
 int meow(int a,int b){
@@ -9,29 +10,78 @@ void foo(){
         _show_line_info_
         meow(3,4);
 }
+// void start_test_cases(int no_of_test_cases){
+//     for (int __ = 0; __ < n; ++__){switch (__){
+//
+//         case 0:{}break;
+//
+//         default:printf("%s\n","No test case executed. Wrong test case number." );break;}}}
+int add(int a,int b){
+        return a+b;
+}
+int tee=4;
+void name() {
+        unwind(1,(char *)&tee,sizeof(tee));
+        printf("%d\n",tee );
+}
+
 int main(int argc, char const *argv[]) {
-
-
-
-        _show_line_info_
-        _show_res_usage_info_
-
-        foo();
-
-        _print_virtmem_usage_
-        _print_mem_usage_
-
-        _start_trace_
-        printf("%d\n", meow(1,2));
-        while (1) {
-                printf("%d\n", meow(1,2));
-                sleep(1);
-                break;
-        }
-        meow(1,2);
-        meow(1,2);
-        _stop_trace_
-
+        _init_cprof_
+        char *aa;
+        // printf("%s\n",aa);
+        int a,b,c;
+        tee=5;
+        name();
+        _show_backtrace_
+        // _start_testing_(2)
+        //
+        //         _start_cases_
+        //                 _start_case_(0)
+        //                         a=1;b=1;
+        //                 _stop_case_
+        //                 _start_case_(1)
+        //                         a=-1;b=1;
+        //                 _stop_case_
+        //         _stop_cases_
+        // ///block we are testing
+        //
+        //         c=add(a,b);
+        // /////end of block
+        //         _start_verification_
+        //                 _start_case_verify_(0)
+        //                         if (c==2) {
+        //                                 printf("test case %d passed \n",__ );
+        //                         }
+        //                 _stop_case_verify
+        //                 _start_case_verify_(1)
+        //                         if (c==0) {
+        //                                 printf("test case %d passed \n",__ );
+        //                         }
+        //                 _stop_case_verify
+        //         _stop_verification_
+        //
+        // _stop_testing_
+        //
+        // show_backtrace();
+        // _show_line_info_
+        // _show_res_usage_info_
+        //
+        // foo();
+        //
+        // _print_virtmem_usage_
+        // _print_mem_usage_
+        //
+        // _start_trace_
+        // printf("%d\n", meow(1,2));
+        // while (1) {
+        //         printf("%d\n", meow(1,2));
+        //         sleep(1);
+        //         break;
+        // }
+        // meow(1,2);
+        // meow(1,2);
+        // _stop_trace_
+        //
 
         printf("%s\n","exiting" );
 
